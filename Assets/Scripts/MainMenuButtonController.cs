3a80cf31ad6a414d424c1d309f49e0c3361faa4d
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2018-06-05 16:29:57 -0700

    furthur scene development

---
+++ Assets/Scripts/MainMenuButtonController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonController : MonoBehaviour {
+
+    public void LoadSceneByName(string _sceneName)
+    {
+        GameStateManager.Instance.ChangeScene(_sceneName);
+    }
+
+}
