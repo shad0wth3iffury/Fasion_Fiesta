f427d942ad5c6c9672e3d50db7dd59b15a5b2c2c
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2018-06-05 17:25:22 -0700

    Preliminary item management

---
+++ Assets/Scripts/ClothingItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothingItem : MonoBehaviour {
+
+    public Texture DisplayImage;
+    public Texture ModelImage;
+    public string TypeName;
+    public Vector2 TypeValue;
+    public Vector2 Color;
+    public Vector2 Trait1;
+
+}
