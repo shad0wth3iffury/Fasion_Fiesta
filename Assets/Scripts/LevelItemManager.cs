5c1c3fbe5ffe24a2c4e9c2e4e1d64005665ffe65
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2018-06-08 11:32:06 -0700

    Object pooling

---
+++ Assets/Scripts/LevelItemManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelItemManager : MonoBehaviour {
+
+    public List<GameObject> ObjectsToPool = new List<GameObject>();
+    public List<Transform> GridPoint = new List<Transform>();
+
+    private void Start()
+    {
+        ObjectPool.Instance.PoolSpawnList(ObjectsToPool);
+        MoveToGrid();
+    }
+
+    void MoveToGrid()
+    {
+        for(int i = 0; i < GridPoint.Count; i++)
+        {
+            GameObject x = ObjectPool.Instance.PooledObjects[i];
+            x.transform.position = GridPoint[i].position;
+            x.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Object Pool.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+
+    public static ObjectPool Instance;
+    public List<GameObject> PooledObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void PoolSpawnList( List<GameObject> _spawnList )
+    {
+        for(int i = 0; i < _spawnList.Count; i++)
+        {
+            GameObject _spawned = Instantiate(_spawnList[i]);
+            PooledObjects.Add(_spawned);
+            _spawned.SetActive(false);
+        }
+    }
+
+    public void ClearObjectPool()
+    {
+        PooledObjects.Clear();
+    }
+
+}
